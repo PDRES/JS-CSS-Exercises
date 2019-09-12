170bc3fbe9dff205fc5b9a2da681aace45acef66
PDRES <47571835+PDRES@users.noreply.github.com>
2019-09-12 11:35:41 +0200

    Add files via upload

---
+++ primero.js
@@ -0,0 +1,31 @@
+// hola tio asi son comentarios en js
+
+
+console.log("Esto es otro comentario,consola");
+
+console.log(5 + 5);
+
+// FUNCIONES
+
+function saludar() {
+    
+    console.log("hola Buenos dias");
+}
+saludar();
+saludar();
+
+//funciones con parametros 
+
+function suma( a, b) {
+  console.log("el resultado es:", a + b)
+}
+suma(5, 9)
+
+//funcion con parametro devuelve un valor 
+function multiplicar(a,b)  {
+    
+        return a * b
+}
+
+
+var sueldo= multiplicar(5, 5)
